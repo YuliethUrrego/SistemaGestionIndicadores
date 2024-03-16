9f4086157ee1682107f9ec90f7eaef70ea3cf2bb
YuliethUrrego <yulieth320rpo@gmail.com>
2024-03-16 16:50:42 -0500

    Vista de Tipo Indicador

--- SistemaGestionIndicadores/Models/TipoIndicador.cs
+++ SistemaGestionIndicadores/Models/TipoIndicador.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Web;
+using System.Text;
 
 namespace SistemaGestionIndicadores.Models
 {
---
+++ SistemaGestionIndicadores/TipoIndicador_View.aspx.cs
@@ -0,0 +1,57 @@
+﻿using SistemaGestionIndicadores.Controllers;
+using SistemaGestionIndicadores.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaGestionIndicadores
+{
+    public partial class TipoIndicador_View : System.Web.UI.Page
+    {
+        protected List<TipoIndicador> listaTipoIndicador = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TipoIndicadorController objTipoIndicadorController = new TipoIndicadorController();
+            listaTipoIndicador = objTipoIndicadorController.List();
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            TipoIndicador objTipoIndicador = new TipoIndicador(nombre);
+            TipoIndicadorController objTipoIndicadorController = new TipoIndicadorController(objTipoIndicador);
+            objTipoIndicadorController.Create();
+            Response.Redirect("TipoIndicador_View.aspx");
+        }
+
+        protected void btnConsultar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            TipoIndicador objTipoIndicador = new TipoIndicador(id);
+            TipoIndicadorController objTipoIndicadorController = new TipoIndicadorController(objTipoIndicador);
+            txtNombre.Text = objTipoIndicadorController.Search().Nombre;
+        }
+
+        protected void btnModificar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            string nombre = txtNombre.Text;
+            TipoIndicador objTipoIndicador = new TipoIndicador(id, nombre);
+            TipoIndicadorController objTipoIndicadorController = new TipoIndicadorController(objTipoIndicador);
+            objTipoIndicadorController.Update();
+            Response.Redirect("TipoIndicador_View.aspx");
+        }
+
+        protected void btnBorrar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            TipoIndicador objTipoIndicador = new TipoIndicador(id);
+            TipoIndicadorController objTipoIndicadorController = new TipoIndicadorController(objTipoIndicador);
+            objTipoIndicadorController.Delete();
+            Response.Redirect("TipoIndicador_View.aspx");
+        }
+    }
+}
